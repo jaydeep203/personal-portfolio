4ce57fc1f6072a271d5ffe54995b8805f70e062d
Jaydeep Deshpande <mrjaydeep2003@gmail.com>
2024-01-07 01:33:58 +0530

    loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React from 'react'
-import { BounceLoader } from 'react-spinners';
+import { ClipLoader } from 'react-spinners';
 
 const Loading = () => {
   return (
-    <div className='h-full w-full z-50 flex items-center justify-center'>
-        <BounceLoader color='#22c55e' size={40} />
+    <div className='h-full w-full flex items-center justify-center'>
+        <ClipLoader color='#22c55e' size={40} />
     </div>
   )
 }
